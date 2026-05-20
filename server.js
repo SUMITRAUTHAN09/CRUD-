7e6d6e27604c68079fc3d0ba6707e32551f87b80
Sumit_Rauthan <sumitrauthan413@gmail.com>
2026-05-20 13:35:37 +0530

    GET, POST, GET by UUID, UUID change the CommanJS into Import

--- server.js
+++ server.js
@@ -1,19 +1,53 @@
-const express = require("express");
-
+import express from "express";
+import { v4 as uuid } from "uuid";
+uuid();
 const app = express();
 const PORT = 3000;
 
+uuid();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get("/test", (req, res) => {
-  const { username, year, subs } = req.query;
-  res.send(`searchin for ${username} ${year} ${subs}`);
+app.set("view engine", "ejs");
+
+const array = [
+  { id: uuid(), username: "rohit", comment: "user name is rohit" },
+  { id: uuid(), username: "mohan", comment: "this is mohans comment" },
+  { id: uuid(), username: "jon", comment: "user jone say hii" },
+  { id: uuid(), username: "ronny", comment: "this is ronny's comment" },
+  { id: uuid(), username: "rocky", comment: "user name is rocky" },
+  { id: uuid(), username: "kamal", comment: "this is ghansala's comment" },
+];
+
+app.get("/", (req, res) => {
+  res.send("the first page ");
+});
+app.get("/comments", (req, res) => {
+  res.render("comments/index", { array });
 });
 
-app.post("/test", (req, res) => {
-  console.log(req.query);
-  const { username, year } = req.body;
-  res.send(`comment was user name: ${username}, year: ${year}`);
+app.get("/comments/new", (req, res) => {
+  res.render("comments/new");
+});
+
+app.post("/comments/new", (req, res) => {
+  console.log(req.body); // debugging
+  const { username, comment } = req.body; // de-structure
+  array.push({ username, comment, id: uuid() });
+  res.send(`<h1>data successfully updated!  just a sec for the result</h1>
+    <script>setTimeout(()=>{
+      window.location.href="/comments";},2000);
+      </script>
+    `);
+
+  console.log(array); // debuging
+});
+
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = array.find((c) => {
+    return c.id === id;
+  });
+  res.render("comments/show", { comment });
 });
 
 app.listen(PORT, () => {
